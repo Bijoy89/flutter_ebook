a2b13fd1c4488c30caac1f52b62840368b82be84
bijoy bhattacharjee <151954402+Bijoy89@users.noreply.github.com>
2025-06-28 20:02:56 +0600

    Firebase Setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ebook/Pages/SplacePage/SplacePage.dart';
 import 'package:flutter_ebook/Pages/WelcomePage.dart';
 import 'package:get/get.dart';
 
 import 'Config/Themes.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(DevicePreview(builder: (context) => flutter_ebookmain()));
   //runApp(const flutter_ebookmain());
 }
